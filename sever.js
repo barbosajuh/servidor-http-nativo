24586cc2e4fbc8ed3396599c6ccc54f84853c06c
Julia <julia.magalhaes@estudante.ifms.edu.br>
2026-08-10 21:20:19 +0000

    servidor criado com sucesso

---
+++ sever.js
@@ -0,0 +1,14 @@
+const http = require('node:http');
+
+const PORTA = 3000;
+
+const sever = http.createServer((req, res) => {
+    console.lo(`requisição recebida! ${req.method} ${req.ur1}`);
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+    res.end("Servidor nativo funcionando!");
+});
+http.Server.listen(PORTA, () => {
+    console.log(`Servidor funcionando na porta ${PORTA}`);
+});
