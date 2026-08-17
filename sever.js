08ac1b2f246c6b1e636cb8bf8a41b8e0de7fef13
Julia <julia.magalhaes@estudante.ifms.edu.br>
2026-08-17 20:26:00 +0000

    ex1' git push
    clear

--- sever.js
+++ sever.js
@@ -5,7 +5,7 @@ const PORTA = 3000;
 const sever = http.createServer((req, res) => {
     console.lo(`requisição recebida! ${req.method} ${req.ur1}`);
 
-    res.statusCode = 200;
+    res.statusCode = 201;
     res.setHeader('Content-Type', 'text/plain; charset=utf-8')
     res.end("Servidor nativo funcionando!");
 });
